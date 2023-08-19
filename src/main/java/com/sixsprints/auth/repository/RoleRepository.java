63b70810364863f1f7feafad2088e39a71274200
Karan Gujral <karan.gujral@jptokyo.co.jp>
2023-08-19 18:34:38 +0530

    Updated Spring boot

--- src/main/java/com/sixsprints/auth/repository/RoleRepository.java
+++ src/main/java/com/sixsprints/auth/repository/RoleRepository.java
@@ -1,11 +1,8 @@
 package com.sixsprints.auth.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.sixsprints.auth.domain.Role;
 import com.sixsprints.core.repository.GenericRepository;
 
-@Repository
 public interface RoleRepository extends GenericRepository<Role> {
 
   Role findByNameIgnoreCase(String name);
