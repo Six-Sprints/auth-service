b6d41dec09ce753b25e6a8980dbf95ae857d7700
Karan Gujral <kgujral@gmail.com>
2025-10-31 01:02:23 +0530

    updated the versions

--- src/test/java/com/sixsprints/auth/controller/BaseControllerTest.java
+++ src/test/java/com/sixsprints/auth/controller/BaseControllerTest.java
@@ -7,14 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.sixsprints.auth.AuthServiceApplication;
+import com.sixsprints.core.utils.MessageSourceHolder;
 
 @SpringBootTest(classes = AuthServiceApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(MessageSourceHolder.class)
 public class BaseControllerTest {
 
   @Autowired
